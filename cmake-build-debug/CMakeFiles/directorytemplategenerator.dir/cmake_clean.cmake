9e5a519cb39de3f5e5d6fd0bb0d92a2013b2f5fd
Vasyl Onufriyev <okvasiliy97@gmail.com>
2019-12-12 18:55:36 -0600

    new project

---
+++ cmake-build-debug/CMakeFiles/directorytemplategenerator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/directorytemplategenerator.dir/main.c.o"
+  "directorytemplategenerator"
+  "directorytemplategenerator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/directorytemplategenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
