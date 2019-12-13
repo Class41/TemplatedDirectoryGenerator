4549c932cf75eb838b5475b5be0fb7f8320d98d9
Vasyl Onufriyev <okvasiliy97@gmail.com>
2019-12-12 23:30:39 -0600

    started adding depth stack

--- inc/parsetemplate.c
+++ inc/parsetemplate.c
@@ -2,8 +2,10 @@
 #include <unistd.h>
 #include "parsetemplate.h"
 #include "stdio.h"
+#include <string.h>
 
 void executeTemplate(char *sourceFile) {
+
     if (access(sourceFile, F_OK) == -1) {
         printf("File %s does not exist or cannot be accessed!", sourceFile);
         return;
@@ -11,13 +13,22 @@ void executeTemplate(char *sourceFile) {
 
     FILE *template = fopen(sourceFile, "r");
 
-    size_t* linen = NULL;
-    char** line = NULL;
-    int linelength = NULL;
+    size_t linen = 0;
+    char *line = NULL;
+    int linelength = 0;
+
+    char **pathstack = NULL;
+    int *psp = 0;
 
     printf("Starting template read...\n");
-    while ((linelength = getline(line, linen, template)) != -1) {
-        printf("Line: %s", line);
+    while ((linelength = getline(&line, &linen, template)) != -1) {
+        int indent = 0;
+        for (int i = 0; i < linelength; i++) {
+            indent = (strcmp(&line[i], '\t') == 0) ? indent++ : indent;
+        }
+        printf("depth: %i\n", indent);
     }
     printf("Folder structure generated\n");
+
+    fclose(template);
 }
